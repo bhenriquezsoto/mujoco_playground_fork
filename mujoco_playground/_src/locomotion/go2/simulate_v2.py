b6c46e5180ccde0278491dd368eed7a08a12de3d
Benjamin Henriquez Soto <benja20029@uc.cl>
2025-12-22 15:59:59 -0300

    feat: add version that uses a timer to handle control loop

--- mujoco_playground/_src/locomotion/go2/simulate_v2.py
+++ mujoco_playground/_src/locomotion/go2/simulate_v2.py
@@ -2,20 +2,23 @@ import os
 from brax.training.agents.ppo import networks as ppo_networks
 from brax.io import model
 from mujoco_playground.config import locomotion_params
-from mujoco_playground import registry, MjxEnv
+from mujoco_playground import registry
 import jax
 from mujoco_playground._src.mjx_env import get_sensor_data
 import mujoco
 import time
 import mujoco.viewer
+import logging
 from mujoco import mjx
-from typing import Any, Dict, List
+from typing import List
 from mujoco_playground._src.locomotion.go2 import go2_constants as consts
 
 
 os.environ['JAX_PLATFORMS'] = 'gpu'
 jax.config.update('jax_platform_name', 'gpu')
 jax.config.update('jax_disable_jit', False)
+# Remove jax debug logging
+logging.getLogger('jax').setLevel(logging.WARNING)
 
 def get_obs_sensors(model: mujoco.MjModel, data: mjx.Data) -> List:
 	gyro = get_sensor_data(model, data, consts.GYRO_SENSOR)
@@ -35,7 +38,7 @@ def import_model(env_name='Go2JoystickFlatTerrain', env_cfg=None):
 	
 	
 	ppo_params = locomotion_params.brax_ppo_config(env_name)
-	model_params = model.load_params('ppo_go2joystick_flatterrain_params_v1')
+	model_params = model.load_params('ppo_go2joystick_flatterrain_params_v3')
 	ppo = ppo_networks.make_ppo_networks(action_size=env_cfg.action_size, observation_size=env_cfg.observation_size, **ppo_params.network_factory)
 	make_inference = ppo_networks.make_inference_fn(ppo)
 	inference_fnTEST = make_inference(model_params)
@@ -43,8 +46,6 @@ def import_model(env_name='Go2JoystickFlatTerrain', env_cfg=None):
 	return inference_fnTEST
 
 if __name__ == "__main__":
-	
-	start = time.time()
 	rng = jax.random.PRNGKey(0)
 	env_name = 'Go2JoystickFlatTerrain'
 	env_cfg = registry.get_default_config(env_name)
@@ -54,34 +55,50 @@ if __name__ == "__main__":
 
 	inference = import_model(env_name, env_cfg)
 	rng = jax.random.PRNGKey(0)
-	command = jax.numpy.array([1.0, 0.0, 0.0])  # Command: "Go forward at 1.0 m/s"
 	last_action = jax.numpy.zeros(env_cfg.action_size)
 	inference = jax.jit(inference)
+	command = jax.numpy.array([0.0, -1.0, 0.0]) 
+	counter_control = 0
+	counter_init = 0
 
 	with mujoco.viewer.launch_passive(m, d) as viewer:
 		# Obtain observations
-
 		mujoco.mj_resetData(m, d)
+		# act_rng, rng = jax.random.split(rng)
+		# obs_sensors = get_obs_sensors(m, d)
+		# obs = jax.numpy.concatenate(obs_sensors + [last_action, command_1])
+		# # Inference from command -> angle off set for each of the joints
+		# ctrl, _ = inference(obs, act_rng)
+		timer_control = time.time()
+		motors_targets = m.keyframe("home").qpos[7:]
 		while viewer.is_running:
+			# print(f"Control dt: {env_cfg.ctrl_dt}s, Sim dt: {env_cfg.sim_dt}s")
 			step_start = time.time()
-			act_rng, rng = jax.random.split(rng)
-			obs_sensors = get_obs_sensors(m, d)
-			obs = jax.numpy.concatenate(obs_sensors + [last_action, command])
-			# Inference from command -> angle off set for each of the joints
-			ctrl, _ = inference(obs, act_rng)
-			motors_targets = m.keyframe("home").qpos[7:] + ctrl * 0.3
-			d.ctrl[:] = motors_targets
-			number_substeps = round(int(env_cfg.ctrl_dt / env_cfg.sim_dt))
-			i = 0
-			for _ in range(number_substeps):
-				mujoco.mj_step(m, d)
-				d.ctrl = motors_targets
-				viewer.sync()
-				time_until_next_step = env_cfg.ctrl_dt - (time.time() - step_start) / (number_substeps *2)
-				# print(time_until_next_step)
-				if time_until_next_step > 0:
-					time.sleep(time_until_next_step)
+			if timer_control > env_cfg.ctrl_dt:
+				act_rng, rng = jax.random.split(rng)
+				# print(f"Time elapsed for random split: {time.time() - step_start:.4f}s")
+				obs_sensors = get_obs_sensors(m, d)
+				# print(f"Time elapsed for getting sensors: {time.time() - step_start:.4f}s")
+				obs = jax.numpy.concatenate(obs_sensors + [last_action, command])
+				# print(f"Time elapsed for concatenating obs: {time.time() - step_start:.4f}s")
+				# Inference from command -> angle off set for each of the joints
+				ctrl, _ = inference(obs, act_rng)
+				# print(f"Time elapsed for inference: {time.time() - step_start:.4f}s")
+				motors_targets = m.keyframe("home").qpos[7:] + ctrl * env_cfg.action_scale
+				# print(f"Time elapsed for getting motor targets: {time.time() - step_start:.4f}s")
+				timer_control = time.time()
+				last_action = ctrl
+				print("Time used in control loop: {:.4f}s".format(time.time() - step_start))
 
-			last_action = ctrl
+			d.ctrl = motors_targets
+			time_until_next_step = env_cfg.sim_dt - (time.time() - step_start)
+			if time_until_next_step > 0:
+				time.sleep(time_until_next_step)
+			else:
+				print(f"Step took longer ({env_cfg.sim_dt - time_until_next_step:.4f}s) than sim_dt of {env_cfg.sim_dt}s")
+				pass
 
+			mujoco.mj_step(m, d)
+			viewer.sync()
+			counter_control += 1
 
